3240477c5a1797fda8c606f083fd7fbe699114a0
HyunWookKim <lentasha9508@gmail.com>
2022-08-15 20:30:16 +0900

    canvas study

---
+++ canvas-draw-img/index.js
@@ -0,0 +1,18 @@
+function copyImageToCanvas(){
+    const image = document.querySelector('img');
+    const canvas = document.getElementById('canvas');
+
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+        image, 0, 0,
+        100, 100,
+        0, 0,
+        64, 64
+    )
+
+}
+
+setTimeout(()=>{
+    copyImageToCanvas();
+}, 3000)
